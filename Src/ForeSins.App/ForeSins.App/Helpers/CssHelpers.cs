dfb6cb22a824a0472a9e8c92304267ee03dc81b2
Sam Mullins <sammullins.co.uk@gmail.com>
2021-08-18 08:34:44 +0100

    Initial commit of blazor app with a home page and a logo.

---
+++ Src/ForeSins.App/ForeSins.App/Helpers/CssHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForeSins.App.Helpers
+{
+    public static class CssHelpers
+    {
+        public static string ToCssClass(this string str)
+        {
+            return str.ToHypenSeparatedFromPascalCase();
+        }
+    }
+}
---
+++ Src/ForeSins.App/ForeSins.App/Helpers/StringHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ForeSins.App.Helpers
+{
+    public static class StringHelpers
+    {
+        public static string ToCamelCaseFromPascalCase(this string str)
+        {
+            if (!string.IsNullOrEmpty(str) && str.Length > 1)
+            {
+                return char.ToLowerInvariant(str[0]) + str.Substring(1);
+            }
+
+            return str;
+        }
+
+        public static string ToHypenSeparatedFromPascalCase(this string str)
+        {
+            return Regex.Replace(str, "[a-z][A-Z]", m => $"{m.Value[0]}-{char.ToLower(m.Value[1])}").ToLower();
+        }
+    }
+}

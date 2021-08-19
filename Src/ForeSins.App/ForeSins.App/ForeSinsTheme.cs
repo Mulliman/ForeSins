67c92dc42cc6e5fceb489ac217240e045beac4b8
Sam Mullins <sammullins.co.uk@gmail.com>
2021-08-19 14:17:12 +0100

    Added some more components.

---
+++ Src/ForeSins.App/ForeSins.App/ForeSinsTheme.cs
@@ -0,0 +1,27 @@
+﻿using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForeSins.App
+{
+    public class ForeSinsTheme
+    {
+        public const string MainRed = "#BF406D";
+        public const string MainGreen = "#4DB258";
+        public const string MainBlue = "#40A9BF";
+
+        public static MudTheme MudTheme => new()
+        {
+            Palette = new Palette()
+            {
+                Primary = MainRed,
+                PrimaryContrastText = Colors.Shades.White,
+                Secondary = MainGreen,
+                Tertiary = MainBlue,
+                AppbarBackground = Colors.Red.Default,
+            }
+        };
+    }
+}

64ff2f1f5cbe4accec49aa32e9a498d15292d913
Sam Mullins <sammullins.co.uk@gmail.com>
2021-08-22 21:41:09 +0100

    Added results section to round page,

---
+++ Src/ForeSins.App/ForeSins.App/Helpers/DateHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForeSins.App.Helpers
+{
+    public static class DateHelpers
+    {
+        public static string ToDateString(this DateTime? date)
+        {
+            return date?.ToString("dd MMM yy");
+        }
+    }
+}

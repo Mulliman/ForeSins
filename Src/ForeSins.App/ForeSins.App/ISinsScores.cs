79ef2135304e4ccfd0d9bdcba220bab474e3ebd9
Sam Mullins <sammullins.co.uk@gmail.com>
2021-08-22 22:50:40 +0100

    Working round entry page.

---
+++ Src/ForeSins.App/ForeSins.App/ISinsScores.cs
@@ -0,0 +1,24 @@
+﻿namespace ForeSins.App
+{
+    public interface ISinsScores
+    {
+        uint SickeningYips { get; set; }
+
+        uint ShockingChips { get; set; }
+
+        uint SplashTroubles { get; set; }
+
+        uint ScrappyDoubles { get; set; }
+
+        uint SquanderedPositions { get; set; }
+
+        uint StupidDecisions { get; set; }
+    }
+
+    public class SinScore
+    {
+        public Sins Sin { get; set; }
+
+        public uint Value { get; set; }
+    }
+}
